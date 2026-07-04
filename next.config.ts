e4e449c9e12089744523a06f124cecc1371d15ee
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2026-07-04 14:23:11 +0530

    Add new dependencies
    Add epub2, pdf-parse to dependencies.
    The need for modification of next.config.ts was that by this way the pdfjs-dist which is internally used by pdf-parse will run as a sepearate file not inside nextjs.
    For more details checkout docs under src/app/_docs

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  serverExternalPackages: ["pdf-parse", "pdfjs-dist", "epub2"],
 };
 
 export default nextConfig;
