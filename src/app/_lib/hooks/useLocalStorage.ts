56ebeb08b838a23ce9bfe42b0b9b83b99785ccbd
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2026-07-16 16:42:23 +0530

    feat(use local storage): create local storage hook

---
+++ src/app/_lib/hooks/useLocalStorage.ts
@@ -0,0 +1,24 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+function useLocalStorage<T>(key: string, initialValue: T) {
+  const [value, setValue] = useState<T>(initialValue);
+
+  useEffect(() => {
+    const saved = window.localStorage.getItem(key);
+
+    if (saved) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setValue(JSON.parse(saved));
+    }
+  }, [key]);
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as const;
+}
+
+export default useLocalStorage;
