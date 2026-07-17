86b9b3cce614f800b40f3ca1c148afe8e8409a61
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2026-07-17 17:38:52 +0530

    feat(settings page): update data layer
    Now data layer has functions for uploading profile picture, getting signed url for that image.

--- src/app/_lib/userDetails.ts
+++ src/app/_lib/userDetails.ts
@@ -3,8 +3,11 @@
 import { db } from "@/app/_lib/db";
 
 import { userDetails } from "@/app/_db/schema";
-
+import { randomUUID } from "crypto";
+import { supabase } from "./supabase";
 import { eq } from "drizzle-orm";
+
+const PLACEHOLDER_USER_IMAGE = "/placeholder-user-image.png";
 
 export async function getUserName(): Promise<string> {
   const [data] = await db
@@ -26,11 +29,67 @@ export async function getUserEmail(): Promise<string> {
   return data?.email || "";
 }
 
-export async function getProfilePicturePath(): Promise<string> {
+async function getProfilePicturePath(): Promise<string> {
   const [data] = await db
     .select({ profilePicturePath: userDetails.profilePicturePath })
     .from(userDetails)
     .where(eq(userDetails.id, 1));
 
-  return data?.profilePicturePath || "/placeholder-user-image.png";
+  return data?.profilePicturePath || PLACEHOLDER_USER_IMAGE;
+}
+
+export async function getProfilePictureSignedUrl(): Promise<string> {
+  const profilePicturePath = await getProfilePicturePath();
+
+  if (profilePicturePath === PLACEHOLDER_USER_IMAGE)
+    return PLACEHOLDER_USER_IMAGE;
+
+  const { data: dataFromStorage, error } = await supabase.storage
+    .from("books")
+    .createSignedUrl(profilePicturePath, 60 * 60);
+
+  return dataFromStorage?.signedUrl || PLACEHOLDER_USER_IMAGE;
+}
+
+export async function updateProfilePicture(file: File): Promise<boolean> {
+  try {
+    const pathFromStorage = await getProfilePicturePath();
+
+    const safeFilename = file.name.replace(/[^a-zA-Z0-9._-]/g, "_");
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    let filePath = "";
+
+    if (pathFromStorage === PLACEHOLDER_USER_IMAGE) {
+      const idForStorage = randomUUID();
+      filePath = `${idForStorage}/${safeFilename}`;
+    } else {
+      const idFromStorage = pathFromStorage.split("/").at(-1);
+      filePath = `${idFromStorage}/${safeFilename}`;
+    }
+
+    const { error: uploadError } = await supabase.storage
+      .from("books")
+      .upload(filePath, buffer, {
+        contentType: file.type,
+        upsert: false,
+      });
+
+    if (uploadError) {
+      console.log("Error while uploading the file", uploadError);
+      return false;
+    }
+
+    await db
+      .update(userDetails)
+      .set({ profilePicturePath: filePath })
+      .where(eq(userDetails.id, 1));
+
+    return true;
+  } catch (err) {
+    console.error("Error occured while updating profile picture", err);
+
+    return false;
+  }
 }
